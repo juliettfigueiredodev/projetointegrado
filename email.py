a432d3caa2eae6ccf1191f2e0ff6c040c5015463
Juliett Figueirêdo <figueiredo.juliett@aluno.ufca.edu.br>
2025-12-10 16:00:52 -0300

    modificacoes: em donor pra adicionar telefone, cpf e email Criacao: new file service_Cpf Precisa de teste: email.py e service_Cpf usando a classe Donor

--- email.py
+++ email.py
@@ -1,10 +1,24 @@
-import re
+import re #módulo de expressões regulares
 
-class email:
-    
+#classe para validar email para as classes Donor e Institution
+class Email: 
+    # antes e depois do @ recebe letras e alguns caracteres especiais, espera-se . e depois apenas letras limitando a quantidade (2 a 3)
+    regex = r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z][2-3]'
+
+    #inicializa o objeto da classe
     def __init__(self, email):
-        self.email = email
+        #apenas guarda o valor se o método de validação retornar True e retorna mensagem
+        if self.valida_email(email): 
+            self.email = email        
+        #retorna mensagem de erro, caso o método retorne False
+        else: raise ValueError ('Email inválido')
+
+    #retorno padrão do email digitado
+    def __str__(self):
+        return self.email
 
     def valida_email(self, email):
-        regex = r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z][2-7]'
-        return 'Email Válido' if re.fullmatch(regex,email) else 'Email inválido'
+         
+        #compara o regex com a entrada e caso email seguir a expressão retorna verdadeiro, caso contrário, retorna falso
+        return bool(self.regex.fullmatch(email))
+        
