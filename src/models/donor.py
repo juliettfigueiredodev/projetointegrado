a8a56f27e10b41280b3784308ae71630f5347c9b
Leandro Pereira <leandro.pereira@aluno.ufca.edu.br>
2025-12-12 19:03:04 -0300

    Update donor.py
    corrige nome do doador (name)

--- src/models/donor.py
+++ src/models/donor.py
@@ -5,7 +5,7 @@ from src.validators import PhoneValidator #validação
 
 class Donor(User): #Herança - classe filha de User
    #inicializa a classe Donor
-   def __init__(self, cpf, donor_name, date_birth, user_id, user_type, email, password, endereco, telefone): 
+   def __init__(self, cpf, name, date_birth, user_id, user_type, email, password, endereco, telefone): 
       # herda atributos da classe mãe
       super().__init__(user_id, user_type, email, password, endereco, telefone) 
       #chama método de validação de Email
@@ -16,7 +16,7 @@ class Donor(User): #Herança - classe filha de User
       #chama método de validação de Telefone
       PhoneValidator.valida_telefone(telefone)
 
-      self.donor_name = donor_name
+      self.name = name
       self.date_birth = date_birth
 
       
