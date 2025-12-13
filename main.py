dcb70b906a7bf0358f46df45f8cd6dd8485877e5
Juan Carlos <116115392+JuanCostaDev@users.noreply.github.com>
2025-12-13 15:22:11 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from src.models.User import *
-
-#AREA DE TESTES, FAVOR IGNORAR
-user1 = User('1', 'donor', 'teste@gmail.com', 12345678, 'rua teste, 512, Crato', 8888888888)
-
-dados = user1.send_to_bd()
-print(dados)
-
-user1.password = "123()8899"
